080adb288382bb03a7f5c39ff157c6de5d4e7277
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-07-10 08:33:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,23 +16,13 @@ window.onload = () =>{
     console.log(err);
   });
 
-
-
 // get and write data from LoaclStorage to main
 let text = localStorage.getItem("testJSON");
 obj = JSON.parse(text);
-
-let new_text = document.createTextNode(obj.data[1].title);
-
-document.getElementById("main_id").appendChild(new_text);
-
-
+build(obj.data.sort(date_sort_desc));
 }
 
 
-
-
- 
 // Sorting by date
 let date_sort_asc = function (item1, item2) {
   if ( Date.parse(item1.createdAt)  >  Date.parse(item2.createdAt)) return 1;
@@ -51,44 +41,116 @@ let date_sort_desc = function (item1, item2) {
 // Sorting by tags
 let tags_sort = function (item1, item2) {
 
-  if (item1.tags_match1 > item2.tags_match2)
+  if (item1.tagOccurs > item2.tagOccurs)
     return -1;
-  if (item1.tags_match1 > item2.tags_match2)
+  if (item1.tagOccurs < item2.tagOccurs)
     return 1;
-
   if ( Date.parse(item1.createdAt)  >  Date.parse(item2.createdAt))
     return - 1;
   if ( Date.parse(item1.createdAt) <  Date.parse(item2.createdAt))
     return 1;
-
 return 0;
 }
 
 
-let choosed_tags = [];
+function build(array ){
+  delete_post();
+  let article_box = document.getElementById("article_box");
+  article_box.innerHTML = "";
+  let one_art = document.createElement('div');
+  
+  for(let i = 0; i < array.length; i++){
+    let title = document.createElement('div');
+    let descr = document.createElement('div');
+    let image = document.createElement('img');
+    let time =  document.createElement('div');
+    let rubbish = document.createElement('div');
+    one_art.setAttribute('class', 'one_art');
+    title.setAttribute('class', 'title');
+    descr.setAttribute('class', 'descr');
+    image.setAttribute('src', 'image');
+    time.setAttribute('class', 'time');
+    rubbish.setAttribute("id", 'rb' + i);
+    rubbish.setAttribute("class", 'rb');
+
+    let t = document.createTextNode(array[i].title);
+    let tt = document.createTextNode(array[i].description);
+    image.setAttribute('src', array[i].image);
+    let ttt = document.createTextNode(beautifyTime(array[i].createdAt));
+    title.appendChild(t);
+    descr.appendChild(tt);
+    time.appendChild(ttt);
+
+    let tags_wrp_article = document.createElement('div');
+    tags_wrp_article.setAttribute('class', 'tags_wrp_article');
+
+    for(let j = 0; j < array[i].tags.length; j++){
+      let tag_article = document.createElement('div');
+      tag_article.setAttribute('class', 'tag_article');
+      let tag = document.createTextNode(array[i].tags[j]);
+      tag_article.appendChild(tag);
+      tags_wrp_article.appendChild(tag_article)
+
+    };
+    one_art.appendChild(title);
+    one_art.appendChild(image);
+    one_art.appendChild(descr);
+    one_art.appendChild(time);
+    one_art.appendChild(tags_wrp_article);
+    one_art.appendChild(rubbish);
+    article_box.appendChild(one_art);
+  }
+  
+
+}
+
+function a_d_tougle(){
+  let date_button = document.getElementById("date_s");
+  if (!date_button.classList.contains("asc_ord")){
+    date_button.classList.add("asc_ord");
+    date_button.classList.remove("desc_ord");
+    build(obj.data.sort(date_sort_asc));
+  }else{
+    date_button.classList.remove("asc_ord");
+    date_button.classList.add("desc_ord");
+    build(obj.data.sort(date_sort_desc));
+
+  }
+}
 
 
+// to compare object
+function compare(object, tags){
+  for (let q = 0; q < 50; q++){
+    let occurs = 0;
+    for(let w = 0; w < object.data[q].tags.length; w++){
+      if (tags.includes(object.data[q].tags[w]) ){
+        // console.log(object.data[q].tags[w]);
+        occurs += 1;
+      }
+    };
+    object.data[q].tagOccurs = occurs;
+    occurs = 0;
+  }
+  return object
+}
+
+// console.log(compare(obj, choosedTags));
 // TAGS query
+let choosedTags = [];
 
 setTimeout(function(){
-  obj.data.sort(date_sort_asc);
-  console.log(obj);
-
-
-
-
 
 // make tagArray
   let tagArray = [];
   for(let i = 0; i < 50; i++){
-    console.log(obj.data[i].tags);
+    // console.log(obj.data[i].tags);
     for(let j = 0; j < obj.data[i].tags.length; j++){
       if (!tagArray.includes(obj.data[i].tags[j]) ){
         tagArray.push(obj.data[i].tags[j]);
       }
     };
   };
-  console.log(tagArray);
   
 
   // create list of elements in page
@@ -101,6 +163,8 @@ setTimeout(function(){
     new_li.setAttribute('id', "tag" + (k+1 ));
     new_li.classList.add('tag');
 
+
+    // add event listener - change colour
     new_li.addEventListener('click', function(e){
       if (!new_li.classList.contains("choosed")){
         new_li.classList.add("choosed");
@@ -109,34 +173,70 @@ setTimeout(function(){
         new_li.classList.remove("choosed");
         new_li.classList.add("unchoosed");
       }
-      // console.log(e.target.id)
 
 
 
-      // add to list of choosed tags;
-      let curr_tag = tagArray[+e.target.id.substr(-1)-1]
-      console.log( curr_tag);
+      // add to list of choosed tags or remove;
+      let curr_tag = tagArray[+e.target.id.substr(-1)-1];
 
-      if (!choosed_tags.includes(curr_tag)){
-        choosed_tags.push(curr_tag);
+      if (!choosedTags.includes(curr_tag)){
+        choosedTags.push(curr_tag);
       }else{
-      choosed_tags.splice((choosed_tags.findIndex(curr_tag)) , 1);
+        choosedTags.splice(choosedTags.indexOf(curr_tag), 1);
       }
-      console.log(choosed_tags);
-
       
+      let bro = compare(obj, choosedTags);
+      build(bro.data.sort(tags_sort));
+
     }, false)
 
 
     tagListNode.appendChild(new_li);
     }
-  document.getElementById("main_id").appendChild(tagListNode);
+  document.getElementById("taglist_wrp").appendChild(tagListNode);
   
 
 
+// put action on Date button
+let date_button = document.getElementById("date_s");
+date_button.addEventListener("click", a_d_tougle, false );
 
-
-
-
-}, 1500
+}, 100
 )
+
+function beautifyTime(timeString){
+  let outPut = ''
+  let date = new Date(timeString);
+              
+  let mm = date.getMonth(); 
+  let dd = date.getDate();
+  let hh = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+              
+  let mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']; 
+              
+  outPut += `${[  (hh>9 ? '' : '0') + hh + 'h', 
+  (min>9 ? '' : '0') + min + 'm', 
+  (sec>9 ? '' : '0') + sec + 's',
+  (dd>9 ? '' : '0') + dd,
+  mS[+((mm>9 ? '' : '0') + mm)],
+  date.getFullYear()
+  ].join(', ')}`
+  return outPut
+}
+
+
+function delete_post(){
+  rubbish_q = document.getElementsByClassName('rb');
+  for(let i = 0; i < rubbish_q.length; i++){
+    
+    let item = rubbish_q[i];
+    item.addEventListener('click', function(e){
+     
+      document.getElementById(e.target.id).parentElement.style.display = 'none';
+    })
+  }
+}
+
+// delete_post(); 
